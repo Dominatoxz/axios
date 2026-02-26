6811502d095361f4e8fa1acc9e5f21c1e870c069
Dominatoxz <dominatomatheus06@gmail.com>
2026-02-26 19:12:38 -0300

    26/02

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, {useEffect, useState} from "react";
+import api from "./services/api";
+import './App.css';
+
+export default function App() {
+    const [username, setUsername] = useState('thaispll');
+    const[user, setUser] = useState(null);
+    const[repos, setRepos] = useState([]);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+    
+    
+    //carrega dados da API
+    useEffect(() =>{
+      async function loadData( userToSearch = username){
+        try{
+          setLoading(true);
+          setError(null);
+
+          //Carrega usuário
+          const userResponse = await api.get(`/users/${userToSearch}`); //API Responde
+          setUser(userResponse.data); //Extrai os dados da API
+
+          const reposResponse = await api.get(`/users/${userToSearch}/repos?per_page=6&sort=updated`); 
+          setRepos(reposResponse.data); 
+
+        } catch(error){
+          console.error('Erro', error);
+          setError(`Usuário ${userToSearch} não encontrado`);
+
+        } finally{
+          setLoading(false);
+        }
+        loadData();
+      }
+    }, []);
+
+    if(loading){
+      return (
+        <div className="loading-container">
+          <div className="spinner"></div>
+          <p>Carregando perfil...</p>
+        </div>
+      );
+    }
+
+    if(error || !user) {
+      return(
+        <div className="error-container">
+          <h2>❌ {error}</h2>
+          <button onClick={ () => window.location.reload()}> Tentar novamente</button>
+        </div>
+      )
+    }
+
+    return(
+      <div className="app-container">
+        <div className="profile-card"></div>
+          <header className="profile-header">
+            <img src={user.avatar_url} alt={user.name} className="avatar"/>
+            <div className="user-info">
+              <h1>{user.name || user.login}</h1>
+              <p>{user.bio} || "Sem bio disponível"</p>
+              <div className="status">
+                <span><strong>{user.followers}</strong> seguidores</span>
+                <span><strong>{user.following}</strong> Seguindo</span>
+              </div>
+              <a href={user.html_url} target="_blank" rel="noreferrer" className="github-link">
+                Ver perfil completo
+              </a>
+            </div>
+          </header>
+      </div>
+    );
+}
+
+
+
